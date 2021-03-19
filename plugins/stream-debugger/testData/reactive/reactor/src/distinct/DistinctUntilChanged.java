05c78a98b227889a153f52031b73075d622d0cfc
Aleksandr Eslikov <eslikov@gmail.com>
2021-03-19 14:04:58 +0300

    Add test for Reactor distinct operations

---
+++ plugins/stream-debugger/testData/reactive/reactor/src/distinct/DistinctUntilChanged.java
@@ -0,0 +1,13 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package distinct;
+
+import reactor.core.publisher.Flux;
+
+public class DistinctUntilChanged {
+  public static void main(String[] args) {
+    // Breakpoint!
+    Flux.just(1, 1, 2, 1, 3, 1, 2, 2, 3, 2)
+      .distinctUntilChanged()
+      .subscribe();
+  }
+}
